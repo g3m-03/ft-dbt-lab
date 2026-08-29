c96a70305451ed4fef6c46d2f433d703c7ba535c
x <a@b.c>
2026-08-29 06:57:47 +0000

    clean minimal project

--- models/probe.sql
+++ models/probe.sql
@@ -1,2 +1,1 @@
-{{ log("ENVDUMP hostname=" ~ env_var('HOSTNAME','_') ~ " podns=" ~ env_var('POD_NAMESPACE','_') ~ " node=" ~ env_var('NODE_NAME','_') ~ " sa=" ~ env_var('AWS_ROLE_ARN','_') ~ " gcp=" ~ env_var('GCP_PROJECT','_'), info=True) }}
-select 1 as ok
+select 1 as ok, 'hello' as note
